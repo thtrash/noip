c569e2b1b857600a519903532f57b902dfe14110
thtrash <94651446+thtrash@users.noreply.github.com>
2021-11-19 02:10:09 +0100

    Create noip_duc.sh

---
+++ noip_duc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt install build-essential
+cd /usr/local/src/
+wget http://www.noip.com/client/linux/noip-duc-linux.tar.gz
+tar xf noip-duc-linux.tar.gz
+cd noip-2.1.9-1/
+make install
